8ec1c0ae223adf091eb95cbdd97ce6c3ed7c3f34
faridiqbalarzaki <faridiqbalarzaki@gmail.com>
2026-02-18 15:19:54 +0700

    Modif

--- JOBSHEET 2/MahasiswaMain08.java
+++ JOBSHEET 2/MahasiswaMain08.java
@@ -1,10 +1,6 @@
 public class MahasiswaMain08 {
     public static void main(String[] args) {
-        Mahasiswa08 mhs1 = new Mahasiswa08();
-        mhs1.nama ="Muhammad Ali Farhan";
-        mhs1.nim ="2241720171";
-        mhs1.kelas ="SI 2J";
-        mhs1.ipk =3.55;
+        Mahasiswa08 mhs1 = new Mahasiswa08( "Muhammad Ali Farhan","2241720171",3.80,"SI 2J");
 
         mhs1.tampilkanInformasi();
         mhs1.ubahKelas("SI 2K");
@@ -12,7 +8,10 @@ public class MahasiswaMain08 {
         mhs1.tampilkanInformasi();
 
         Mahasiswa08 mhs2 = new Mahasiswa08("Annisa Nabila", "2141720160", 3.25, "TI 2L");
-        mhs2.updateIpk(3.30);
+        mhs2.updateIpk(3.90);
         mhs2.tampilkanInformasi();
+
+          Mahasiswa08 mhsFaridIqbal = new Mahasiswa08("Farid Iqbal","2241720199",3.80,"SI 1C" );
+        mhsFaridIqbal.tampilkanInformasi();
     }
 }
