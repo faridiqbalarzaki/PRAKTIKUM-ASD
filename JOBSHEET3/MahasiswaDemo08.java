9840d97b11a70f388fc7938a4b5eb309cd1997af
faridiqbalarzaki <faridiqbalarzaki@gmail.com>
2026-02-25 09:05:42 +0700

    Jobsheet 3

---
+++ JOBSHEET3/MahasiswaDemo08.java
@@ -0,0 +1,35 @@
+package JOBSHEET3;
+import java.util.Scanner;
+public class MahasiswaDemo08 {
+    
+ public static void main(String[] args) {
+Scanner  sc =new Scanner(System.in);
+        Mahasiswa08 [] arrayOfMahasiswa08 = new Mahasiswa08[3];
+        String dummy;
+        for (int i=0; i < 3; i++){
+            arrayOfMahasiswa08[i] = new Mahasiswa08();
+
+            System.out.println("Masukan Data Mahasiswa ke-"+(i+1));
+            System.out.print("Masukan NIM     : ");
+            arrayOfMahasiswa08[i].nim = sc.nextLine();
+            System.out.print("Masukan Nama    : ");
+            arrayOfMahasiswa08[i].nama = sc.nextLine();
+            System.out.print("Masukan Kelas   : ");
+            arrayOfMahasiswa08[i].kelas = sc.nextLine();
+            System.out.print("Masukan IPK     : ");
+            dummy = sc.nextLine();
+            arrayOfMahasiswa08[i].ipk = Float.parseFloat(dummy);
+            System.out.println("------------------------------------------");
+        }
+    
+for (int i = 0; i < arrayOfMahasiswa08.length; i++) {
+    System.out.println("Data Mahasiswa ke-"+(i+1));
+    System.out.println("NIM     : "+arrayOfMahasiswa08[i].nim);
+        System.out.println("NAMA    : "+arrayOfMahasiswa08[i].nama);
+        System.out.println("KELAS   : "+arrayOfMahasiswa08[i].kelas);
+        System.out.println("IPK     : "+arrayOfMahasiswa08[i].ipk);
+        System.out.println("------------------------------------------------");
+}
+
+    }
+}
