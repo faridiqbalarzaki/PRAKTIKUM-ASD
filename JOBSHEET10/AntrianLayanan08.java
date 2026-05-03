409b444b5771cfda4729ecd4cffce3e8d0084d00
faridiqbalarzaki <faridiqbalarzaki@gmail.com>
2026-05-03 13:00:58 +0700

    2.2. Percobaan 2 : Antrian Layanan Akademik

---
+++ JOBSHEET10/AntrianLayanan08.java
@@ -0,0 +1,75 @@
+package JOBSHEET10;
+
+public class AntrianLayanan08 {
+    Mahasiswa08[] data;
+    int front;
+    int rear;
+    int size;
+    int max;
+
+    public AntrianLayanan08(int max) {
+        this.max = max;
+        this.data = new Mahasiswa08[max];
+        this.front = 0;
+        this.rear = -1;
+        this.size = 0;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean isFull() {
+        return size == max;
+    }
+
+    public void tambahAntrian(Mahasiswa08 mhs) {
+        if (isFull()) {
+            System.out.println("Antrian penuh, tidak dapat menambah mahasiswa.");
+            return;
+        }
+        rear = (rear + 1) % max;
+        data[rear] = mhs;
+        size++;
+        System.out.println(mhs.nama + " berhasil masuk ke antrian.");
+    }
+
+    public Mahasiswa08 layaniMahasiswa() {
+        if (isEmpty()) {
+            System.out.println("Antrian kosong.");
+            return null;
+        }
+        Mahasiswa08 mhs = data[front];
+        front = (front + 1) % max;
+        size--;
+        return mhs;
+    }
+
+    public void lihatTerdepan() {
+        if (isEmpty()) {
+            System.out.println("Antrian kosong.");
+        } else {
+            System.out.print("Mahasiswa terdepan: ");
+            System.out.println("NIM \t NAMA \t PRODI \t KELAS");
+            data[front].tampilkanData();
+        }
+    }
+
+    public void tampilkanSemua() {
+        if (isEmpty()) {
+            System.out.println("Antrian kosong.");
+            return;
+        }
+        System.out.println("Daftar Mahasiswa dalam Antrian:");
+        System.out.println("NIM \t NAMA \t PRODI \t KELAS");
+        for (int i = 0; i < size; i++) {
+            int index = (front + i) % max;
+            System.out.print((i + 1) + ". ");
+            data[index].tampilkanData();
+        }
+    }
+
+    public int getJumlahAntrian() {
+        return size;
+    }
+}
