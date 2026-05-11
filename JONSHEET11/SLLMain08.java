a922de7d98867e57e50a0456f2544c1f817abe79
faridiqbalarzaki <faridiqbalarzaki@gmail.com>
2026-05-11 09:57:09 +0700

    Modif soal 2.1.2

--- JONSHEET11/SLLMain08.java
+++ JONSHEET11/SLLMain08.java
@@ -1,21 +1,33 @@
 package JONSHEET11;
 
+import java.util.Scanner;
+
 public class SLLMain08 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         SingleLinkedList08 sll = new SingleLinkedList08();
 
-        Mahasiswa08 mhs1 = new Mahasiswa08("24212200", "Alvaro", "1A", 4.0);
-        Mahasiswa08 mhs2 = new Mahasiswa08("23212201", "Bimon", "2B", 3.8);
-        Mahasiswa08 mhs3 = new Mahasiswa08("22212202", "Cintia", "3C", 3.5);
-        Mahasiswa08 mhs4 = new Mahasiswa08("21212203", "Dirga", "40", 3.6);
+        System.out.print("Masukkan Jumlah data Mahasiswa yang ingin diinput: ");
+        int n = sc.nextInt();
+        sc.nextLine();
 
-        sll.print();
-        sll.addFirst(mhs4);
-        sll.print();
-        sll.addLast(mhs1);
-        sll.print();
-        sll.insertAfter("Dirga", mhs3);
-        sll.insertAt(2, mhs2);
+        for (int i = 1; i <= n; i++) {
+            System.out.println("Masukkan data Mahasiswa ke-" + i);
+            System.out.print("NIM: ");
+            String nim = sc.nextLine();
+            System.out.print("Nama: ");
+            String nama = sc.nextLine();
+            System.out.print("Kelas: ");
+            String kelas = sc.nextLine();
+            System.out.print("IPK: ");
+            double ipk = sc.nextDouble();
+            sc.nextLine();
+
+            Mahasiswa08 mhsBaru = new Mahasiswa08(nim, nama, kelas, ipk);
+            sll.addLast(mhsBaru);
+        }
+
+        System.out.println("\n=== Data Antrian Mahasiswa ===");
         sll.print();
     }
 }
